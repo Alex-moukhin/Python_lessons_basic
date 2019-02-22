1c7296d048b80bc811ebab7465fe32e08e0bcabe
Alex_moukhin <moukhin@r-broker.ru>
2019-02-22 14:05:28 +0300

    Мухин Алексей moukhin@3-broker.ru

--- lesson03/home_work/hw03_hard.py
+++ lesson03/home_work/hw03_hard.py
@@ -9,6 +9,9 @@
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
 
+'''
+не хватило врвмени ((( 
+'''
 
 # Задание-2:
 # Дана ведомость расчета заработной платы (файл "data/workers").
@@ -17,6 +20,61 @@
 # то их ЗП уменьшается пропорционально, а за заждый час переработки
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
+
+import re
+# составляем лист из файла workers
+data_workers = []
+workers = open('workers.txt', 'r', encoding='utf-8')
+for line in workers:
+    data_workers.append(re.findall(r'[А-я]+[_]?[А-я]+|[0-9]+', line))
+workers.close()
+# удаялем заголовок таблицы
+del data_workers[0]
+
+# составляем лист из файла hours
+hours = open('hours_of.txt', 'r', encoding='utf-8')
+data_hours = []
+for line in hours:
+    data_hours.append(re.findall(r'[А-я]+[_]?[А-я]+|[0-9]+', line))
+hours.close()
+# удаялем заголовок таблицы
+del data_hours[0]
+
+# создаем файл, куда будем записывать результат
+result_salary = open('result_salary.txt', 'w', encoding='utf-8')
+result_salary.write('Имя Фамилия Заработанная плата')
+'''
+Алгоритм расчета:
+1. Добавляем переменные, в которые записываем имя и фамилию
+2. Проводим сравнение в двух листах и сопоставляем строчки с данными
+3. Добавляем переменные, обозначая данные из разных файлов
+4. Прописываем алгоритм расчета зарпалаты согласно заданию
+5. Записываем результат расчета зарплат в файл
+
+'''
+for i in data_workers:
+    name_workers = i[0]
+    surname_workers= i[1]
+    for n in data_hours:
+        name_in_data_hours = n[0]
+        surname_in_data_hours = n[1]
+        if name_workers == name_in_data_hours and surname_workers == surname_in_data_hours:
+            salary = int(i[2])
+            hours_norm = int(i[4])
+            hours_fact = int(n[2])
+            work = hours_fact - hours_norm
+            if work > 0:
+                price = salary + (2 * (salary / hours_norm) * (hours_work - hours_norm))
+            else:
+                price = salary + (salary / hours_norm) * (hours_work - hours_norm)
+            # записываем в файл квитанции
+            person_data = '{0} {1} {2:.2f}\n'.format(name_workers,
+                                                  surname_workers,
+                                                  price)
+            result_salary.write(person_data)
+            print(person_data.strip())
+result_salary.close()
+print('Зарплатная ведомость сформирована в файле - result_salary.txt')
 
 
 # Задание-3:
@@ -31,3 +89,24 @@
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+fruits_dict = dict()
+with open('fruits.txt',encoding='utf-8') as inp_ut: 
+# записываем строки из файла в line
+    line = inp_ut.readlines()
+# убираем пробелы в строках
+    line = list(filter(lambda x: x!='\n', line))
+    for normal_line in line:
+# записываем словарь со списом названий файлов и значениями, соответствующий первым буквам слов
+        file_name = 'fruits_{}'.format(normal_line[0].upper())
+        fruits_dict[file_name] = fruits_dict.get(file_name,'')+normal_line
+    
+# цикл записи файла
+for i in fruits_dict:
+# записываем в переменную формат названия файла
+    name = '{}.txt'.format(i)
+# открываем файл на зпись
+    with open(name,'w') as out:
+# записываем значения из словаря в файл
+        out.write(fruits_dict[i])
+print('Формирование файлов по именам фруктов закончено!')
